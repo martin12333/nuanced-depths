a0985acbf73f9f7fd17e2736edf568a8536c4a5e
Aisha <Tessa@garys-mbp-104.home>
2016-03-02 19:12:19 +0000

    initial commit

---
+++ tokipona_to_english.py
@@ -0,0 +1,172 @@
+# the toki pona dictionary as simple as I could make it!
+tpdictionary = {
+'a': 'ah!',
+'akesi': 'reptile',
+'ala': 'none',
+'alasa': 'hunt or gather',
+'ale': 'all',
+'anpa': 'under',
+'ante': 'different',
+'anu': 'or',
+'awen': 'wait',
+'e': 'the',
+'en': 'and',
+'esun': 'shop',
+'ijo': 'thing',
+'ike': 'bad',
+'ilo': 'tool',
+'insa': 'inside',
+'jaki': 'yucky',
+'jan': 'person',
+'jelo': 'yellow',
+'jo': 'have',
+'kala': 'fishy',
+'kalama': 'make noise',
+'kama': 'come',
+'kasi': 'leaf',
+'ken': 'can',
+'kepeken': 'use',
+'kili': 'fruit',
+'kin': 'also',
+'kipisi': 'cut',
+'kiwen': 'hard stone',
+'ko': 'squishy',
+'kon': 'wind/smell/soul',
+'kule': 'colour',
+'kulupu': 'club',
+'kute': 'listen',
+'la': 'it is *goes between phrase of context and sentence*',
+'lape': 'sleep',
+'laso': 'blue-green',
+'lawa': 'head',
+'len': 'clothes',
+'lete': 'cold',
+'li': 'is',
+'lili': 'little',
+'linja': 'line',
+'lipu': 'paper',
+'loje': 'red',
+'lon': 'here',
+'luka': 'hand (or five if counting numbers)',
+'lukin': 'look at',
+'lupa': 'hole',
+'ma': 'land',
+'mama': 'parent',
+'mani': 'money',
+'meli': 'female',
+'mi': 'me',
+'mije': 'man',
+'moku': 'food',
+'moli': 'death',
+'monsi': 'behind',
+'mu': 'moo!',
+'mun': 'moon',
+'musi': 'recreation',
+'mute': 'many',
+'namako': 'embellish',
+'nanpa': 'number',
+'nasa': 'crazy',
+'nasin': 'way',
+'nena': 'hill or nose',
+'net': 'Internet',
+'ni': 'this',
+'nimi': 'name',
+'noka': 'leg or foot',
+'o': 'hey!',
+'oko': 'eye',
+'olin': 'love',
+'ona': 'they',
+'open': 'open',
+'pakala': 'accident',
+'pali': 'work',
+'palisa': 'stick',
+'pan': 'carbohydrate',
+'pana': 'give',
+'pi': 'of',
+'pilin': 'feeling',
+'pimeja': 'dark',
+'pini': 'end',
+'pipi': 'bug',
+'poka': 'next to',
+'poki': 'bowl',
+'pona': 'good',
+'pu': 'The Official toki pona Book',
+'sama': 'same',
+'seli': 'heat',
+'selo': 'outside',
+'seme': 'what',
+'sewi': 'above',
+'sijelo': 'body',
+'sike': 'circle',
+'sin': 'new',
+'sina': 'you',
+'sinpin': 'front or wall',
+'sitelen': 'picture or writing',
+'sona': 'know',
+'soweli': 'land animal',
+'suli': 'big',
+'suno': 'sun',
+'supa': 'flat surface',
+'suwi': 'sweet',
+'tan': 'from',
+'taso': 'only or but',
+'tawa': 'towards',
+'telo': 'liquid',
+'tenpo': 'time',
+'toki': 'communication',
+'tomo': 'house',
+'tu': 'two',
+'unpa': 'sex',
+'uta': 'mouth',
+'utala': 'conflict',
+'walo': 'white',
+'wan': 'one',
+'waso': 'birdy',
+'wawa': 'power',
+'weka': 'away',
+'wile': 'want',
+}
+
+# functions
+
+def OneWordTranslate():
+    tpword = input("\n sona ala ni anu seme? ")
+    englishword = tpdictionary.get(tpword,"*nothing in toki pona!*")
+    print("\n", tpword, 'means', englishword)
+    again = input("\n lookup another, \'y\' or \'n\'? ")
+    if again == "y":
+        OneWordTranslate()
+    else:
+        end = input("\n close program")
+
+def SentanceTranslate():
+    tpsentenceList = str(input("\n sona ala ni anu seme? "))
+    tpsentence = tpsentenceList.split()
+    englishsentece = []
+    for x in tpsentence:
+        tpword = tpdictionary.get(x,"nothing in toki pona!")
+        englishsentece.append(tpword)
+    print("\n", ' '.join(englishsentece))
+    again = input("\n translate another, \'y\' or \'n\'? ")
+    if again == "y":
+        SentanceTranslate()
+    else:
+        end = input("\n close program")
+
+def choice():
+    WordOrSentence = input("translate sentence \'s\' or lookup word \'w\'? ")
+    if WordOrSentence == "w":
+        OneWordTranslate()   
+    elif WordOrSentence == "s":
+        SentanceTranslate()
+    else:
+        print("seme?")
+        choice()
+ 
+ # translate toki pona to english!
+ 
+choice()
+
+#print("\n")
+#for tpword, englishword in tpdictionary():
+#    print(key, " means ", value)
